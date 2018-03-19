ec05e0d0592d2b55a8f849cedfcf6bb723ac21cb
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-20 09:24:45 +1100

    update style, granny talk

--- granny_talk/gran.js
+++ granny_talk/gran.js
@@ -1,19 +1,34 @@
 console.log('Granny Talk')
 
-
-var randomYear = function(min, max) {
+var randomNum = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 } // creates range and then adds back base to return a number in our wanted range. could use round() instead of floor and '+1', this reduces end probabilities
+
+
+var vowels = ['A','E','I','O','U']
+var grandpaTalk = function(string) {
+  for (var i = 0; i < string.length; i++) {
+    for (var j = 0; j < vowels.length; j++) {
+      if (string[i] === vowels[j]){
+        newString[i] = string[i].replace(vowel[j], vowel[randomNum(0,4)])
+      } else {
+        newString[i] = string[i]
+      }
+    }
+  }
+}
+
+
 
 var question = prompt('Hello honey. What do you want to ask your dear old gran?')
 
 while (question !== 'BYE') {
   if (question === question.toUpperCase()) {
-    alert('NO, NOT SINCE ' + randomYear(1930, 1950))
-    question = prompt(`What's that darling?`)
-    // question = prompt(`NO, NOT SINCE ${randomYear(1930, 1950)}\nWhat\'s that darling?`)
+    question = prompt('NO, NOT SINCE ' + randomNum(1930, 1950) + '\nWhat\'s that darling?')
+    console.log(grandpaTalk(question))
   } else {
     question = prompt('SPEAK UP HONEY!')
+    console.log(grandpaTalk(question))
   }
 }
 
@@ -21,24 +36,3 @@ alert('What\'s that honey, I didn\'t hear you.. zzz')
 
 
 
-// var vowels = ['A','E','I','O','U']
-// var vowelMix = function(string) {
-//   for (i=0;i<string.length; i++) {
-//     var newString = string.replace(vowels[i], vowels[randomYear(0,vowels.length-1)])
-  
-//   }
-//   return newString
-// }
-
-// var grandpaTalk = function() {
-//   while (question !== 'BYE') {
-//   if (question === question.toUpperCase()) {
-
-//     question = prompt(`NO, NOT SINCE ${randomYear(1930, 1950)}\nWhat\'s that darling?`)
-//   } else {
-//     question = prompt('SPEAK UP HONEY!')
-//   }
-// }
-
-// alert('What\'s that honey, I didn\'t hear you.. zzz')
-// }

0536f931997ed9a2aca32e93e80552721535c1d4
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-21 09:50:34 +1100

    create minigolf totaller

---
+++ minigolf/minigolf.js
@@ -0,0 +1,41 @@
+console.log("Mini golf!")
+
+// var bob   = [3, 2, 6, 11, 9, 2, 6, 9, 10]
+// var jimbo = [5, 12, 9, 22, 13, 7, 16, 10, 11]
+// var fish  = [2, 2, 4, 5, 4, 3, 6, 4, 1]
+
+var total = function(total, number) {
+  return total + number //total is the accumulated value, number is the next number in the array
+} 
+
+var players = [
+  { name: "Bob",
+    card: [3, 2, 6, 11, 9, 2, 6, 9, 10]
+  },
+  { name: "Jimbo",
+    card: [5, 12, 9, 22, 13, 7, 16, 10, 11]
+  },
+  { name: "Fish",
+    card: [2, 2, 4, 5, 4, 3, 6, 4, 1]
+  }
+]
+
+var par = [3, 4, 5, 5, 3, 3, 4,3,5]
+var parGame = par.reduce(total)
+
+console.log("Course par is " + parGame)
+
+for (i = 0; i < players.length; i++) {
+  var result = players[i].card.reduce(total)
+  console.log('Player: ' + players[i].name + ' \nScore is: ' + result)
+  var final = result - parGame
+  if (result > parGame) {
+    console.log(final + " over par") 
+  } else if (result === parGame) {
+    console.log("perfect game") 
+  } else if (result < parGame) {
+    console.log(final * -1 + " under par")
+  }
+}
+
+

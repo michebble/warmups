53b74dd563e3031086090aadb366ab4499b0d034
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-22 11:19:21 +1100

    other checkboard pattern generator

--- the_board/board.js
+++ the_board/board.js
@@ -2,6 +2,7 @@ var boardSize = 16
 var boardPattern = "# " 
 var boardArr = []
 var printArr = []
+var grid = ""
 
 // for (var i = 0; i < boardSize; i++) {
 //   boardArr.push(boardPattern)
@@ -11,17 +12,32 @@ var printArr = []
 line = boardPattern.repeat(boardSize)
 
 
-for (var i = 0; i < boardSize; i++) {
-  if (i % 2 === 0) {
-    printArr.push(line)
+// for (var i = 0; i < boardSize; i++) {
+//   if (i % 2 === 0) {
+//     printArr.push(line)
+//   } else {
+//     printArr.push(line.split("").reverse().join(""))
+//   }
+//   printArr.push("\n")
+// }
+
+// var boardStr = printArr.join("")
+// console.log(boardStr)
+
+for (i = 0; i < boardSize; i++) {
+  grid += (boardPattern.repeat(boardSize) + "\n")
+  if (boardPattern === "# ") {
+    boardPattern = " #"
   } else {
-    printArr.push(line.split("").reverse().join(""))
+    boardPattern = "# "
   }
-  printArr.push("\n")
 }
 
-var boardStr = printArr.join("")
-console.log(boardStr)
+console.log(grid)
+
+
+
+
 
 
 

741763f036426ebd152da58bb751d7ef6bf5865e
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-25 14:24:22 +1100

    add reset, change icons

--- coinflip/decider.js
+++ coinflip/decider.js
@@ -1,6 +1,5 @@
 console.log('time to flip')
 
-
 var coinImg = document.getElementById('coin-img');
 var headsTally = document.querySelector('.heads-tally');
 var tailsTally = document.querySelector('.tails-tally');
@@ -8,10 +7,12 @@ var coinFlipBtn = document.querySelector('.coin-flip-btn');
 var coinSection = document.querySelector('section');
 var heading = document.querySelector('h1');
 var messageHeading = document.querySelector('h3');
-// var fiveFlipBtn = document.querySelector('.five-flip-btn');
+var resetBtn = document.querySelector('.reset-btn');
 
 headsTally.textContent = 0;
 tailsTally.textContent = 0;
+resetBtn.disabled = true;
+messageHeading.textContent = 'First to Five Wins!';
 
 var coinFlip = function() {
   return Math.round(Math.random());
@@ -29,31 +30,42 @@ var plusOne = function(tally) {
   tally.textContent = Number(tally.textContent) + 1;
 }
 
-
+var resetGame = function() {
+  headsTally.textContent = 0;
+  tailsTally.textContent = 0;
+  resetBtn.disabled = true;
+  coinFlipBtn.disabled = false;
+  messageHeading.textContent = ('First to Five Wins!')
+}
 
 var tossIt = function() {
   var toss = coinFlip();
   if (toss === 1) {
     plusOne(headsTally);
-    imgFlip('heads');
+    imgFlip('heads-icon');
     changeColor('#F2463E')
     if (Number(headsTally.textContent) >= 5) { 
-      messageHeading.textContent = 'heads wins!'
+      messageHeading.textContent = 'heads wins!';
+      coinFlipBtn.disabled = true;
+      resetBtn.disabled = false;
     }
   } else if (toss === 0) {
     plusOne(tailsTally)
-    imgFlip('tails')
+    imgFlip('tails-icon')
     changeColor('#3E9EF2');
     if (Number(tailsTally.textContent) >= 5) { 
-      messageHeading.textContent = 'tails wins!'
+      messageHeading.textContent = 'tails wins!';
+      coinFlipBtn.disabled = true;
+      resetBtn.disabled = false;
     }
   }
-
 }
  
 
 
+
 coinFlipBtn.addEventListener('click', tossIt);
+resetBtn.addEventListener('click', resetGame);
 
 
 

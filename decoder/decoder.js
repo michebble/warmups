2ee58b03d8c16b960c932362ec8ee454169e146e
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-26 14:52:16 +1100

    not sure

--- decoder/decoder.js
+++ decoder/decoder.js
@@ -1,26 +1,51 @@
 console.log('the real code buster');
 
-var codeMessage = 'FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV, WKH YDOLDQW QHYHU WDVWH RI GHDWK EXW RQFH.';
+// var codeMessage = 'FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV, WKH YDOLDQW QHYHU WDVWH RI GHDWK EXW RQFH.';
 
-var baseAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// var message = 'TWO SYMBOLS KISSING EACH OTHER'
+
+var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+var offset = 3;
 
 var codeKey = 'DEFGHIJKLMNOPQRSTUVWXYZABC';
 
-codeMessage = codeMessage.split('');
+var input = document.querySelector('.input-message');
+var offset = document.querySelector('.offset-number');
+var output = document.querySelector('.output-message');
+var offetBtn = document.querySelector('.set-offset');
+var encodeBtn = document.querySelector('.encode-btn');
+var decodeBtn = document.querySelector('.decode-btn');
 
 baseAlpha = baseAlpha.split('');
 
-codeKey = codeKey.split('');
+keygenb {
+  var newkey = baseAlpha.slice(0, offset)
 
-var decodedMessage = [];
-
-for (var i = 0; i < codeMessage.length; i++) {
-  if (codeKey.indexOf(codeMessage[i]) !== -1) {
-    var a = codeKey.indexOf(codeMessage[i]);
-    decodedMessage.push(baseAlpha[a]);
-  } else {
-    decodedMessage.push(codeMessage[i]);
-  }
 }
 
-console.log(decodedMessage.join(''));
+var swapLetters = function(inputAplha, outputAlpha) {
+  var message = input.value.split('');
+  var newMessage = [];
+  for (var i = 0; i < message.length; i++) {
+    if (inputAplha.indexOf(message[i]) !== -1) {
+      var newLetter = inputAplha.indexOf(message[i]);
+      newMessage.push(outputAlpha[newLetter]);
+    } else {
+      newMessage.push(message[i]);
+    }
+  }
+  output.textContent = newMessage.join('');
+}
+
+var decoder = function() {
+  swapLetters(codeKey, baseAlpha);
+}
+
+var encoder = function() {
+  swapLetters(baseAlpha, codeKey);
+}
+
+// offsetBtn.addEventListener('click', keyGen);
+encodeBtn.addEventListener('click', encoder);
+decodeBtn.addEventListener('click', decoder);

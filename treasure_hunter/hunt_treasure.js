8a536211d77d440ab1cfbe3a2e9ff0a96d2c8fbb
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-05 11:04:08 +1000

    add js solution

--- treasure_hunter/hunt_treasure.js
+++ treasure_hunter/hunt_treasure.js
@@ -1,48 +1,45 @@
-map1 = [
+var map1 = [
   ["A","A","A","A","A"],
   ["A","A","A","A","A"],
   ["A","A","X","A","A"],
   ["A","A","A","A","A"],
   ["A","A","A","A","A"]
-]
+];
 
-map2 = [
+var map2 = [
   ["A","A","A","A","A"],
   ["A","A","A","A","A"],
   ["A","A","A","A","A"],
   ["A","A","A","A","A"],
   ["A","X","A","A","A"]
-]
+];
   
-map3 = [
+var map3 = [
   ["A","A","A","A","A"],
   ["A","A","V","A","A"],
   ["A","A","A","V","A"],
   ["v","v","V","v","X"],
   ["A","V","A","A","A"]
-]
+];
   
-map4 = [
+var map4 = [
   ["A","A","A","A","A"],
   ["A","A","V","A","A"],
   ["A","A","A","V","A"],
   ["v","v","V","v","A"],
   ["X","V","A","A","A"]
-]
+];
 
-require 'pry'
-map_to_scan = map2
-treasure_mark = "X"
-result = 0
-y_cord = 0
-count = 0
+var mapToScan = map1;
 
-map_to_scan.each_with_index do | row | 
-  if row.include?(treasure_mark)
-  x_cord = row.index(treasure_mark)
-  y_cord = count
-  end
-  count += 1
-end
+var result = [];
 
-puts "[#{y_cord}, #{x_cord}]"
+
+mapToScan.forEach(function(row, row_index) {
+  if (row.includes("X")) {
+    result.push(row_index);
+    result.push(row.indexOf("X"));
+  }
+})
+
+console.log(result)

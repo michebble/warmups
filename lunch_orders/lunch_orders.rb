210458923aa69aff62f5fe410c3739ef5186c49e
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-06 15:30:22 +1000

    complete bonus

--- lunch_orders/lunch_orders.rb
+++ lunch_orders/lunch_orders.rb
@@ -1,6 +1,6 @@
 order = {
-  :name => '',
-  :items => []
+  name: '',
+  items: []
 }
 
 puts "Welcome to lunch land. Please complete your order."
@@ -11,8 +11,18 @@ loop do
   order[:items].push  gets.chomp
   print "Add another item to the order? (y/n): "
   answer = gets.chomp
-  if answer[0].downcase != 'y'
+  if answer[0].downcase != "y"
     break
   end
 end
-puts "All orders: #{order[:items]}"
+
+if order[:items].length > 2
+  order_list = order[:items].join(", ")
+  order_list = order_list.reverse!
+  order_list[order_list.index(" ,")] = " & "
+  order_list = order_list.reverse!
+else
+  order_list = order[:items].join(", ")
+end
+
+puts " #{order[:name]} ordered a #{order_list}"

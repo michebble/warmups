afdeddea0e67a657f26456a4030118e5b7206985
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-10 10:39:34 +1000

    clean up ruby square sorter

--- squaring/get_square.rb
+++ squaring/get_square.rb
@@ -1,17 +1,20 @@
 def get_squares(array_of_numbers)
-  collected_squares =  collect_squares(array_of_numbers)
+  collected_squares =  find_true_squares(array_of_numbers)
   collected_squares.uniq!
   return collected_squares.sort!
 end
 
-def collect_squares(array_of_numbers)
-  true_squares = []
-  array_of_numbers.each do |number|
-      if Math.sqrt(number) % 1 == 0
-        true_squares << number
-      end 
-  end
-  return true_squares
+def find_true_squares(array_of_numbers)
+  return array_of_numbers.select! { |number| Math.sqrt(number) % 1 == 0}
 end
+# def collect_squares(array_of_numbers)
+#   true_squares = []
+#   array_of_numbers.each do |number|
+#       if Math.sqrt(number) % 1 == 0
+#         true_squares << number
+#       end 
+#   end
+#   return true_squares
+# end
 
 puts get_squares([4, 1, 16, 1, 10, 35, 22]) # => [1, 4, 16]

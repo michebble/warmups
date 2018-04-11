2a919f27319c392125b126ee970645aa27605c91
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-12 09:46:16 +1000

    add reset, instruction count to robot

--- 05_robots/robots.rb
+++ 05_robots/robots.rb
@@ -3,12 +3,24 @@ require'pry'
 class Robot
   def initialize
     @name = robot_name
+    @name_calls = 0
+    @instruction_calls = 0
   end
 
   def name
-    puts @name
+    if @name_calls == 2
+      reset
+    else
+      puts @name
+      @name_calls += 1
+    end
+    @instruction_calls += 1
   end
 
+  def instruction_count
+    puts @instruction_calls
+    @instruction_calls += 1
+  end
 
   private
 
@@ -22,13 +34,20 @@ class Robot
   def random_upcase
     (65 + rand(25)).chr
   end
+
   def random_single_num
     rand(0..9).to_s
   end
 
+  def reset
+    puts "Resetting to factory settings."
+    @name = robot_name
+    @name_calls = 0
+    @instruction_calls += 1
+  end
 
 end
 
 binding.pry
 
-puts 'byebye'
+puts 'byebye' 

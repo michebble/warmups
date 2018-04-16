3c7db7ee1cf77f80f4c9be6b84ee6d3062392bc1
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-16 10:15:14 +1000

    broke .roll but create .roll.total

--- 06_dice/dice.rb
+++ 06_dice/dice.rb
@@ -1,18 +1,28 @@
 require'pry'
 
 class Dice 
+  
+
 
 
   class <<self
-    def roll(number)
-      set_of_rolls = []
-      number.to_i.times {set_of_rolls << roll_once}
-      return set_of_rolls
+    def roll(number=1)
+      @set_of_rolls = []
+      number.to_i.times {@set_of_rolls << roll_once}
+      self
     end
 
     def roll_once
       return rand(1..6)
     end
+
+    def total
+      puts "[#{@set_of_rolls}, #{@set_of_rolls.sum}]"
+    end
+
+    
+    
+
   end
 end
 
@@ -22,3 +32,9 @@ binding.pry
 puts
 puts 'farewell'
 puts
+
+# ---------
+# | *   * |
+# |   *   |
+# | *   * |
+# ---------

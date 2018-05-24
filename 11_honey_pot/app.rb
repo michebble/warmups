4fcf7f47583906dad1728b6c7c83f6b665a536a2
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-24 10:42:47 +1000

    honey pot checker

---
+++ 11_honey_pot/app.rb
@@ -0,0 +1,16 @@
+
+def check_honey_pots
+  honey_pots = Array.new(100, false)
+
+  1.upto(honey_pots.length) do |factor|
+    honey_pots.each_index do |index|
+      honey_pots[index] ^= true if (index+1) % factor == 0
+    end
+  end
+
+  honey_pots.each_index
+    .select{|index| honey_pots[index]}
+    .map{|index| index + 1}
+end
+
+p check_honey_pots
